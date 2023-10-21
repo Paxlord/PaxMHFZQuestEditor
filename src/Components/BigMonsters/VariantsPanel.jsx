83771a3a2ffdb7d40092c9367620c17775d2ef86
Paxlord <faresboukhezna@gmail.com>
2023-10-21 05:31:08 +0200

    feat (VariantsPanel) : add mob flags 3/4/5

--- src/Components/BigMonsters/VariantsPanel.jsx
+++ src/Components/BigMonsters/VariantsPanel.jsx
@@ -7,7 +7,7 @@ import SelectComponent from "../../Components/Form/SelectComponent";
 import Panel from "../../Components/Panel";
 import { DifficultiesOptions } from "../../Data/difficulties";
 import { useImmer } from 'use-immer';
-import { ReadMonsterVariant1, ReadMonsterVariant2, WriteMonsterVariants } from '../../Utils/QuestParams/variant_utils';
+import { ReadMonsterVariant1, ReadMonsterVariant2, ReadMonsterVariant3, ReadMonsterVariant4, ReadMonsterVariant5, WriteMonsterVariants } from '../../Utils/QuestParams/variant_utils';
 import { PanelTitle } from '../StyledComponents';
 
 
@@ -17,9 +17,12 @@ const VariantsPanel = () => {
 
   const [variant1, setMonsterVariant] = useState(() => ReadMonsterVariant1(questDataView));
   const [variant2, setMonsterVariant2] = useState(() => ReadMonsterVariant2(questDataView));
+  const [variant3, setMonsterVariant3] = useState(() => ReadMonsterVariant3(questDataView));
+  const [variant4, setMonsterVariant4] = useState(() => ReadMonsterVariant4(questDataView));
+  const [variant5, setMonsterVariant5] = useState(() => ReadMonsterVariant5(questDataView));
 
   const onSave = () => {
-    let newDv = WriteMonsterVariants(questDataView, variant1, variant2);
+    let newDv = WriteMonsterVariants(questDataView, variant1, variant2, variant3, variant4, variant5);
     setQuestDataView(newDv);
   }
 
@@ -29,6 +32,9 @@ const VariantsPanel = () => {
         <div className="flex gap-x-3 mt-1 items-center">
           <NumeralInput label="Flag 1" defaultValue={variant1} onChange={(value) => setMonsterVariant(parseInt(value))} />
           <NumeralInput label="Flag 2" defaultValue={variant2} onChange={(value) => setMonsterVariant2(parseInt(value))} />
+          <NumeralInput label="Flag 3 (Interception)" defaultValue={variant3} onChange={(value) => setMonsterVariant3(parseInt(value))} />
+          <NumeralInput label="Flag 4 (Interception)" defaultValue={variant4} onChange={(value) => setMonsterVariant4(parseInt(value))} />
+          <NumeralInput label="Flag 5 (Interception)" defaultValue={variant5} onChange={(value) => setMonsterVariant5(parseInt(value))} />
         </div>
       </Panel>
   )
--- src/Utils/QuestParams/variant_utils.js
+++ src/Utils/QuestParams/variant_utils.js
@@ -12,9 +12,24 @@ export const ReadMonsterVariant2 = (dataview) => {
   return ReadMonsterVariant(dataview, 0x152);
 }
 
-export const WriteMonsterVariants = (dataview, variant1, variant2) => {
+export const ReadMonsterVariant3 = (dataview) => {
+  return ReadMonsterVariant(dataview, 0x176);
+}
+
+export const ReadMonsterVariant4 = (dataview) => {
+  return ReadMonsterVariant(dataview, 0x177);
+}
+
+export const ReadMonsterVariant5 = (dataview) => {
+  return ReadMonsterVariant(dataview, 0x178);
+}
+
+export const WriteMonsterVariants = (dataview, variant1, variant2, variant3, variant4, variant5) => {
   let dv = immu_write_ubyte(dataview, 0x151, variant1);
   dv = immu_write_ubyte(dv, 0x152, variant2);
+  dv = immu_write_ubyte(dv, 0x176, variant3);
+  dv = immu_write_ubyte(dv, 0x177, variant4);
+  dv = immu_write_ubyte(dv, 0x178, variant5);
   return dv;
 }
 
