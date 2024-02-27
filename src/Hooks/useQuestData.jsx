2ae0f021ffed0f86d9935d7fc65b23e6a8b3eed3
Paxlord <faresboukhezna@gmail.com>
2024-02-27 16:56:08 +0100

    save 3 files instead of 6

--- src/Hooks/useQuestData.jsx
+++ src/Hooks/useQuestData.jsx
@@ -2,14 +2,13 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useContext, createContext } from "react";
 import { save } from "@tauri-apps/api/dialog";
-import { invoke } from '@tauri-apps/api';
+import { invoke } from "@tauri-apps/api";
 
 const QuestDataContext = createContext(null);
 
 export const useQuestData = () => useContext(QuestDataContext);
 
-export const QuestDataProvider = ({children}) => {
-
+export const QuestDataProvider = ({ children }) => {
   const [questBuffer, setQuestBuffer] = useState(null);
   const [questArrayData, setQuestArrayData] = useState(null);
   const [questDataView, setQuestDataView] = useState(null);
@@ -22,55 +21,70 @@ export const QuestDataProvider = ({children}) => {
     let dataview = new DataView(bufferArray.buffer);
     setQuestDataView(dataview);
     setOriginalQuestId(ogfilename);
-  }
+  };
 
   const resetData = () => {
     setQuestDataView(null);
-  }
+  };
 
   const saveDataToFile = async (multiple) => {
-
     let fileName = await save({
-      filters: [{
-        name: 'Quest Binary',
-        extensions: ['bin']
-      }]
+      filters: [
+        {
+          name: "Quest Binary",
+          extensions: ["bin"],
+        },
+      ],
     });
 
     console.log("filename ", fileName);
 
-    const dotIdx= fileName.lastIndexOf('.');
+    const dotIdx = fileName.lastIndexOf(".");
 
-    let uintArray = new Uint8Array(questDataView.buffer) 
+    let uintArray = new Uint8Array(questDataView.buffer);
     let simpleArray = Array.from(uintArray);
 
-    if(fileName){
-
-      if(multiple){
-        await invoke("save_byte_array_to_file", { path: fileName.substring(0, dotIdx) + "d0" + ".bin", bytes: simpleArray})
-        await invoke("save_byte_array_to_file", { path: fileName.substring(0, dotIdx) + "d1" + ".bin", bytes: simpleArray})
-        await invoke("save_byte_array_to_file", { path: fileName.substring(0, dotIdx) + "d2" + ".bin", bytes: simpleArray})
-        await invoke("save_byte_array_to_file", { path: fileName.substring(0, dotIdx) + "n0" + ".bin", bytes: simpleArray})
-        await invoke("save_byte_array_to_file", { path: fileName.substring(0, dotIdx) + "n1" + ".bin", bytes: simpleArray})
-        await invoke("save_byte_array_to_file", { path: fileName.substring(0, dotIdx) + "n2" + ".bin", bytes: simpleArray})
-      }else{
-        await invoke("save_byte_array_to_file", { path: fileName, bytes: simpleArray})
+    if (fileName) {
+      if (multiple) {
+        await invoke("save_byte_array_to_file", {
+          path: fileName.substring(0, dotIdx) + "0" + ".bin",
+          bytes: simpleArray,
+        });
+        await invoke("save_byte_array_to_file", {
+          path: fileName.substring(0, dotIdx) + "1" + ".bin",
+          bytes: simpleArray,
+        });
+        await invoke("save_byte_array_to_file", {
+          path: fileName.substring(0, dotIdx) + "2" + ".bin",
+          bytes: simpleArray,
+        });
+      } else {
+        await invoke("save_byte_array_to_file", {
+          path: fileName,
+          bytes: simpleArray,
+        });
       }
-
     }
-
-  }
+  };
 
   useEffect(() => {
-    if(questDataView){
+    if (questDataView) {
       console.log("Quest Data View Changed : ", questDataView);
     }
-  }, [questDataView])
-  
-  return(
-    <QuestDataContext.Provider value={{ questDataView, originalQuestId, initData, setQuestDataView, resetData, saveDataToFile}}>
+  }, [questDataView]);
+
+  return (
+    <QuestDataContext.Provider
+      value={{
+        questDataView,
+        originalQuestId,
+        initData,
+        setQuestDataView,
+        resetData,
+        saveDataToFile,
+      }}
+    >
       {children}
     </QuestDataContext.Provider>
-  )
-
-}
+  );
+};
