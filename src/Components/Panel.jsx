26d8eb055f6dd681085b06394d8416a9082da1b1
Paxlord <faresboukhezna@gmail.com>
2024-02-27 19:39:53 +0100

    restored save button

--- src/Components/Panel.jsx
+++ src/Components/Panel.jsx
@@ -5,7 +5,12 @@ const Panel = ({ children, onSave, onRevert }) => {
 
       <div className="flex justify-end items-center mt-3">
         {/* <span onClick={() => onRevert()}className="mr-4 text-gray-700 hover:underline cursor-pointer">Revert</span> */}
-        {/* <button onClick={() => onSave()} className="transition px-4 py-1 hover:shadow-md bg-green-500 shadow-sm rounded text-white hover:bg-green-600 active:bg-green-700">Save</button> */}
+        <button
+          onClick={() => onSave()}
+          className="transition px-4 py-1 hover:shadow-md bg-green-500 shadow-sm rounded text-white hover:bg-green-600 active:bg-green-700"
+        >
+          Save
+        </button>
       </div>
     </div>
   );
