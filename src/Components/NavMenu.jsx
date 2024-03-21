495a4dd224345cfb6e56cb34a345a4f89b5cd99a
Paxlord <faresboukhezna@gmail.com>
2024-03-21 18:09:20 +0100

    add category array

--- src/Components/NavMenu.jsx
+++ src/Components/NavMenu.jsx
@@ -1,9 +1,21 @@
-const NavCategory = () => {
-  return <div>a</div>;
+import { useState } from "react";
+
+const categories = ["Quest", "Monsters", "Equipment", "Map"];
+
+const NavCategory = ({ children }) => {
+  return <div>{children}</div>;
 };
 
 const NavMenu = () => {
-  return <nav></nav>;
+  const [selectedCategory, setSelectedCategory] = useState(0);
+
+  return (
+    <nav>
+      {categories.map((title) => (
+        <NavCategory>{title}</NavCategory>
+      ))}
+    </nav>
+  );
 };
 
 export default NavMenu;
